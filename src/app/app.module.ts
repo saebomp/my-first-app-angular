c3a6d28f74a526aa5b6ae73ffa229d791d33a8f3
SAEBOMPAK <sbpark198405@gmail.com>
2022-10-03 14:11:40 -0700

    initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,3 +14,6 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// app.module.ts: this is a file where we tell Angular which pieces belong to our app and there we have to add something to import another package from Angular
