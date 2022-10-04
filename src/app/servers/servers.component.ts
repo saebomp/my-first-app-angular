54728908234e8bba62147defa3add394b00c9b63
SAEBOMPAK <sbpark198405@gmail.com>
2022-10-04 15:29:56 -0700

    add styling

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // selector: '[app-servers]', attribute style
+  //selector: '.app-servers',
+  template: '<app-server></app-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
