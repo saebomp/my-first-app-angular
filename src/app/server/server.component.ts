0c8aa499fe1a654a20f4044a9861cd7ed2671f45
SAEBOMPAK <sbpark198405@gmail.com>
2022-10-04 14:23:46 -0700

    add server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector:'app-server',
+    templateUrl:'./server.component.html'
+})
+export class ServerComponent {
+    
+}
+
+// decorators (@Component) are a typescript feature which allow you to enhance elements you use your code
